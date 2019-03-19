28dc141448d597d85ec862bd273bf78a8b1288aa
Dan Morse <djmthedev@gmail.com>
2019-03-19 06:52:31 -0700

    connected stepper and prepared updateQuantity function

--- VendingMachine/ViewController.swift
+++ VendingMachine/ViewController.swift
@@ -93,6 +93,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         priceLabel.text = "$0.00"
     }
     
+    
+    
+    @IBAction func updateQuantity(_ sender: UIStepper) {
+        print(sender.value)
+    }
+    
+    
+    
     // MARK: UICollectionViewDataSource
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
